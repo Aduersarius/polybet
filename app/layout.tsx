6b0daa6a62fa243df5378ee8a13632f5e0733833
Nikolay Golovin <golovin_nv@magnit.ru>
2025-11-21 02:25:03 +0300

    Add Vercel Analytics and Speed Insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import { Roboto } from "next/font/google";
 import "./globals.css";
 import { Providers } from "./providers";
+import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from '@vercel/speed-insights/next';
 
 const roboto = Roboto({
   weight: ['300', '400', '500', '700'],
@@ -25,6 +27,8 @@ export default function RootLayout({
         <Providers>
           {children}
         </Providers>
+        <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
