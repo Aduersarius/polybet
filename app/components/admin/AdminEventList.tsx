76f0394195982d7d7119484f3f8a44d598327b39
Nikolay Golovin <golovin_nv@magnit.ru>
2025-12-02 21:36:25 +0300

    uyfu

--- app/components/admin/AdminEventList.tsx
+++ app/components/admin/AdminEventList.tsx
@@ -4,6 +4,7 @@ import { useState, useMemo } from 'react';
 import { useQuery, useMutation, useQueryClient } from '@tanstack/react-query';
 import { Pagination } from './Pagination';
 
+
 interface AdminEvent {
     id: string;
     title: string;
